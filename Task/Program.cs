29e778be3501b82ffae3807de240c87b639b3987
Konstantin <kostya.cherevan@gmail.com>
2023-03-15 00:12:15 +0300

    написал код

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"start", "1234","321" };
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+
+
+Console.WriteLine("=====");
+
+
+string[] copy = new string[array.Length];
+
+Array.Copy(array, copy, array.Length);
+
+for (int i = 0; i < copy.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+    Console.WriteLine(copy[i]);
+    }
+}
